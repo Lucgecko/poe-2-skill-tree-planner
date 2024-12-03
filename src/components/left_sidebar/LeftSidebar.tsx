eb4fe03ec08581460f316b6b8d51ad78a0950b5d
Luc <111713888+Lucgecko@users.noreply.github.com>
2024-12-03 00:47:40 -0800

    Update LeftSidebar.tsxgeckorepositoryadded2

--- src/components/left_sidebar/LeftSidebar.tsx
+++ src/components/left_sidebar/LeftSidebar.tsx
@@ -227,6 +227,7 @@ interface LeftSidebarProps {
           <a href="https://github.com/marcoaaguiar/poe2-tree" target="_blank" className="text-blue-500 hover:text-blue-700 font-semibold text-l">
             And Here
           </a>
+          <br></br>
           <a href="https://github.com/Lucgecko/poe-2-skill-tree-planner" target="_blank" className="text-blue-500 hover:text-blue-700 font-semibold text-l">
             Lucgecko's Repository
           </a>
